d3024b8215286f25ae5d642143ee928f1ecaafc2
Michael Solati <mkslt04@gmail.com>
2025-05-29 16:44:44 -0700

    feat: export tts types

--- src/inworld_sdk/__init__.py
+++ src/inworld_sdk/__init__.py
@@ -5,5 +5,7 @@ Inworld AI SDK - A Python SDK for Inworld AI
 __version__ = "0.0.1"
 
 from .client import InworldAIClient
+from .typings.tts import TTSLanguageCodes
+from .typings.tts import TTSVoices
 
-__all__ = ["InworldAIClient"]
+__all__ = ["InworldAIClient", "TTSLanguageCodes", "TTSVoices"]
